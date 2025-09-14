d08e6c454c4c0f7088bd52383760a583a1642b5b
Bartosz Formanowski <bartoszformanowski@gmail.com>
2025-09-14 13:00:48 +0200

    fix: remove leftover code

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -60,24 +60,6 @@ function Navbar() {
         </Flex>
       </Container>
     </Box>
-    // <Flex
-    //   display={display}
-    //   justify="space-between"
-    //   position="sticky"
-    //   color="white"
-    //   align="center"
-    //   bg="bg.muted"
-    //   w="100%"
-    //   top={0}
-    //   p={4}
-    // >
-    //   <Link to="/">
-    //     <Image src={Logo} alt="Logo" maxW="3xs" p={2} />
-    //   </Link>
-    //   <Flex gap={2} alignItems="center">
-    //     <UserMenu />
-    //   </Flex>
-    // </Flex>
   )
 }
 
